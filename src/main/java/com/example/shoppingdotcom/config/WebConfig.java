b1d9ad2fff3ec177ed7f9d1db6b4f15e80dfd4c5
Abhinav Ratna <abhinavratna91@gmail.com>
2024-10-08 07:33:35 +0530

    missed configuration annotation for WebConfig

--- src/main/java/com/example/shoppingdotcom/config/WebConfig.java
+++ src/main/java/com/example/shoppingdotcom/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.example.shoppingdotcom.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
