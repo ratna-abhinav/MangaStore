be2e27679219ed9e0820626f046c1e233e17b188
Abhinav Ratna <abhinavratna91@gmail.com>
2024-10-08 07:37:38 +0530

    fixing profile image is not showing

--- src/main/java/com/example/shoppingdotcom/config/WebConfig.java
+++ src/main/java/com/example/shoppingdotcom/config/WebConfig.java
@@ -11,14 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Serve files from /uploads/profile_img/ as /profile_img/** URLs
         registry.addResourceHandler("/profile_img/**")
-                .addResourceLocations("file:" + System.getProperty("user.home") + "/uploads/profile_img/");
+                .addResourceLocations(System.getProperty("user.home") + "/uploads/profile_img/");
 
         // Serve files from /uploads/product_img/ as /product_img/** URLs
         registry.addResourceHandler("/product_img/**")
-                .addResourceLocations("file:" + System.getProperty("user.home") + "/uploads/product_img/");
+                .addResourceLocations(System.getProperty("user.home") + "/uploads/product_img/");
 
         // Serve files from /uploads/category_img/ as /category_img/** URLs
         registry.addResourceHandler("/category_img/**")
-                .addResourceLocations("file:" + System.getProperty("user.home") + "/uploads/category_img/");
+                .addResourceLocations(System.getProperty("user.home") + "/uploads/category_img/");
     }
 }
